a8c26a5307809f97309bb88be91008751e4df144
Amin Seifi <aminseiifi@gmail.com>
2024-04-03 17:21:19 +0330

    changed candidate state vote reqeust

--- skylab/consensus/state.py
+++ skylab/consensus/state.py
@@ -172,7 +172,7 @@ class CandidateState(State):
         if term > self.consensus_service.current_term:
             self.consensus_service.current_term = term
             self.consensus_service.state = FollowerState(consensus_service=self.consensus_service)
-            return self.consensus_service.current_term, False
+            return self.consensus_service.current_term, True
 
         if (self.consensus_service.voted_for is None or self.consensus_service.voted_for == candidate_id) and \
                 (last_log_term == self.consensus_service.log[-1].term and
